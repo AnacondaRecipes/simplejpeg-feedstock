ea75d6d3fc4262c39792ea50b98d317c1684f4ee
Serhii Kupriienko <skupriienko@anaconda.com>
2026-05-21 10:47:49 +0300

    Update the recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,7 @@
 @echo on
 setlocal enabledelayedexpansion
 
+set SIMPLEJPEG_DYNAMIC_LINKING=1
 copy %RECIPE_DIR%\setup.py .
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
+SIMPLEJPEG_DYNAMIC_LINKING=1
+
 cp "${RECIPE_DIR}/setup.py" .
 "${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation
